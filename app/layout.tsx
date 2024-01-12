9684f521c216eeda4945b88847904e6614577204
Pema Wangchuk <pemawangchug1986@gmail.com>
2024-01-12 15:10:17 +0600

    added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,28 @@
 import { ClerkProvider } from '@clerk/nextjs';
 import React from 'react';
+import { Inter, Space_Grotesk } from 'next/font/google';
+import './globals.css';
 
-export const metadata = {
+import type { Metadata } from 'next';
+import { ThemeProvider } from '@/context/ThemeProvider';
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter',
+});
+const spaceGrotesk = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk',
+});
+
+export const metadata: Metadata = {
   title: 'NextJS PemaStarAi',
+  description: 'A Community Drive Platform for the accountants',
+  icons: {
+    icon: '/assets/images/site-logo.svg',
+  },
 };
 
 export default function RootLayout({
@@ -11,10 +31,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang='en'>
-        <body>{children}</body>
-      </html>
-    </ClerkProvider>
+    <html lang='en'>
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: 'primary-gradient',
+              footerActionLink: 'primary-text-gradient hover:text-primary-500',
+            },
+          }}
+        >
+          <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
