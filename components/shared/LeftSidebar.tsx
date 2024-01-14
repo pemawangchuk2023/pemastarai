6a50ec7b2a6da86e8fef0fb3788f5938e9e98327
Pema Wangchuk <pemawangchug1986@gmail.com>
2024-01-14 19:59:07 +0600

    added webhook

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -79,7 +79,7 @@ const LeftSidebar = () => {
                 height={20}
                 className='invert-colors lg:hidden'
               />
-              <span className='max-lg:hidden'>Sign Out</span>
+              <span className='max-lg:hidden'>Sign Up</span>
             </Button>
           </Link>
         </div>
