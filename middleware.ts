9b331265f8f8dc8e37ca0ba606c644cda55b6652
Pema Wangchuk <pemawangchug1986@gmail.com>
2024-01-15 00:38:10 +0600

    added

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from '@clerk/nextjs';
 export default authMiddleware({
   publicRoutes: [
     '/',
-    '/api/webhook/clerk',
+    '/api/webhook',
     'question/:id',
     '/tags',
     '/tags/:id',
