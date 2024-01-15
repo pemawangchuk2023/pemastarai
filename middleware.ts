f8ddd93cfae9df5a4d47af7d0214ea3a6695a64b
Pema Wangchuk <pemawangchug1986@gmail.com>
2024-01-15 19:27:13 +0600

    changes done

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { authMiddleware } from '@clerk/nextjs';
 export default authMiddleware({
   publicRoutes: [
     '/',
-    '/api/clerk',
+    '/api/webhook',
     'question/:id',
     '/tags',
     '/tags/:id',
@@ -12,7 +12,7 @@ export default authMiddleware({
     '/community',
     '/jobs',
   ],
-  ignoredRoutes: ['/api/clerk', '/api/chatgpt'],
+  ignoredRoutes: ['/api/webhook', '/api/chatgpt'],
 });
 
 export const config = {
