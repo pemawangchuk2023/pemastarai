fb5f94db5c34a1c2f2cbbc18836b2ae3cc9b72c0
Pema Wangchuk <pemawangchug1986@gmail.com>
2024-01-15 20:29:30 +0600

    daded

--- middleware.ts
+++ middleware.ts
@@ -5,12 +5,12 @@ export default authMiddleware({
   publicRoutes: [
     '/',
     '/api/webhook',
-    // 'question/:id',
-    // '/tags',
-    // '/tags/:id',
-    // '/profile/:id',
-    // '/community',
-    // '/jobs',
+    'question/:id',
+    '/tags',
+    '/tags/:id',
+    '/profile/:id',
+    '/community',
+    '/jobs',
   ],
   ignoredRoutes: ['/api/webhook', '/api/chatgpt'],
 });
