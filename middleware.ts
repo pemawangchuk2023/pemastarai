82e5a660c18c4f50af7ba08017c9b0b004d23e83
Pema Wangchuk <pemawangchug1986@gmail.com>
2024-01-16 12:14:12 +0600

    added again

--- middleware.ts
+++ middleware.ts
@@ -12,7 +12,7 @@ export default authMiddleware({
     '/community',
     '/jobs',
   ],
-  // ignoredRoutes: ['/api/webhook', '/api/chatgpt'],
+  ignoredRoutes: ['/api/webhook', '/api/chatgpt'],
 });
 
 export const config = {
