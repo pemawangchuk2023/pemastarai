5ba3ce03675a48f562623edbf6398bcfaa5036fc
Pema Wangchuk <pemawangchug1986@gmail.com>
2024-01-17 12:23:44 +0600

    correced

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,20 @@
+// Middleware for clerk/nextjs
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes: ['/api/webhooks(.*)'],
-  ignoredRoutes: ['/api/webhooks'],
+  publicRoutes: [
+    '/',
+    '/api/webhook',
+    'question/:id',
+    '/tags',
+    '/tags/:id',
+    '/profile/:id',
+    '/community',
+    '/jobs',
+  ],
+  ignoredRoutes: ['/api/webhook', '/api/chatgpt'],
 });
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
