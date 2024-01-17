675106eca757bc27fd6eaaa3fce28ed67016ab8f
Pema Wangchuk <pemawangchug1986@gmail.com>
2024-01-17 22:35:39 +0600

    back

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,18 @@
-// Temporarily remove authMiddleware
-// export default authMiddleware({
-//   publicRoutes: [...],
-// });
+import { authMiddleware } from '@clerk/nextjs';
+
+export default authMiddleware({
+  publicRoutes: [
+    '/',
+    '/api/webhook',
+    'question/:id',
+    '/tags',
+    '/tags/:id',
+    '/profile/:id',
+    '/community',
+    '/jobs',
+  ],
+  ignoredRoutes: ['/api/webhook', '/api/chatgpt'],
+});
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
