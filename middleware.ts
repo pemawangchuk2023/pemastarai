008d1a94123d322b17da36d02fc40618a29844d5
Pema Wangchuk <pemawangchug1986@gmail.com>
2024-01-17 12:06:19 +0600

    aded

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
   publicRoutes: ['/api/webhooks(.*)'],
+  ignoredRoutes: ['/api/webhooks'],
 });
 
 export const config = {
